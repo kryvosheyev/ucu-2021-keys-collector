dfd1225905c93975c2a49628e6bf9f06bd1e2e3f
Serhii Abarovskyi <aserhii@protonmail.com>
2021-12-31 14:28:31 +0200

    Add RXPy

--- crawler/github/settings.py
+++ crawler/github/settings.py
@@ -18,6 +18,7 @@ USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Ge
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
+LOG_LEVEL = "DEBUG"
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
--- crawler/github/spiders/github_api.py
+++ crawler/github/spiders/github_api.py
@@ -1,16 +1,30 @@
 import json
+import asyncio
+import httpx
 
 import scrapy
+import rx
+from rx import operators as ops
 
+from rx.scheduler.eventloop import AsyncIOScheduler
+# scrapy crawl github_api -a q="aws" -a key="dsfrefregf"
 
-class GithubApiSpider(scrapy.Spider):
+class GithubApiSpider:
     name = "github_api"
     handle_httpstatus_list = [400, 403, 404]
 
+    def __init__(self, key, q):
+        self.key = key
+        self.q = q
+        self.start_requests()
+
     def start_requests(self):
-        url = f"https://api.github.com/search/code?q={self.q}"
-        headers = {"Authorization": f"Token {self.key}"}
-        yield scrapy.Request(url, headers=headers, callback=self.get_data)
+        print("started")
+        rx.interval(5).pipe(
+            ops.flat_map(self.scrap_data),
+            ops.map(self.organize_data),
+            ops.flat_map(self.send_data),
+        ).subscribe(scheduler=AsyncIOScheduler(loop))
 
     @staticmethod
     def organize_data(item):
@@ -26,29 +40,48 @@ class GithubApiSpider(scrapy.Spider):
         }
         return data
 
-    def get_data(self, response):
-        resp = response.json()
-        items = resp.get("items")
-        self.logger.debug(f"len of items is {len(items)}")
-        ready_data = map(self.organize_data, items)
-        headers = {"Content-Type": "application/json"}
-        for data in ready_data:
-            yield scrapy.Request(
-                url="http://3.142.70.26:4001/parser/download-and-parse-file",
-                method="POST",
-                dont_filter=True,
-                meta=data,
-                body=json.dumps(data),
-                headers=headers,
-                callback=self.yield_data,
-            )
+    async def make_request(self, observer): 
+        url = f"https://api.github.com/search/code?q={self.q}"
+        headers = {"Authorization": f"Token {self.key}"}
+        async with httpx.AsyncClient() as client:
+            try:
+                response = await client.get(url, headers=headers)
+                if (response.state !== 200)1
+                resp = response.json()
+                items = resp["items"]
+                for rawData in items:
+                    observer.on_next(rawData)
 
-    def yield_data(self, response):
-        yield {
-            "project": response.meta.get("project"),
-            "fileUrl": response.meta.get("fileUrl"),
-            "fileHash": response.meta.get("sha"),
-            "language": response.meta.get("language"),
-        }
-        if response.status != 200:
-            raise Exception(f"{response.url} is {response.status}")
+                observer.on_complete()
+            except Exception as e:
+                observer.on_error(e)
+
+    def scrap_data(self, tick): 
+        return rx.defer(lambda: rx.create(lambda o, s: loop.create_task(self.make_request(o))))
+                # .pipe(
+                #     ops.timeout(10),
+                #     ops.retry(),
+                # )
+                # TODO добавить таймауты, ретраи, добавить случайный ключ
+
+    def send_data(self, data): 
+        return rx.create(
+            lambda observer, scheduler: loop.create_task(self.http_send_data(data, observer))
+        )
+
+    async def http_send_data(self, data, observer): 
+        async with httpx.AsyncClient() as client:
+            response = await client.post("http://3.142.70.26:4001/parser/download-and-parse-file", data=json.dumps(data), headers={"Content-Type": "application/json"})
+            print(response)
+            observer.on_complete()
+
+
+
+async def main(loop):
+    GithubApiSpider("github key", "awsaccess")
+
+
+
+loop = asyncio.get_event_loop()
+loop.create_task(main(loop))
+loop.run_forever()
